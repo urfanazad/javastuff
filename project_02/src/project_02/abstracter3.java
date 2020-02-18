2214c75da15c13f9ef35d4c58bfb170c33163a68
Urfan <urfanazad@hotmail.com>
2020-02-18 11:41:55 +0000

    yo

---
+++ project_02/src/project_02/abstracter3.java
@@ -0,0 +1,18 @@
+package project_02;
+
+public class abstracter3 implements interface1
+{
+public void f()
+{
+System.out.println("I am getting it Deepak");
+}
+public void f2()
+{
+System.out.println("this is so good thanks D your a GURU");	
+}
+public static void main(String[] args) {
+	abstracter3 obj=new abstracter3();
+	obj.f();
+	obj.f2();
+}
+}
